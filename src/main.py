0f37192a6940b7db3b127a932c66dd45cbb32d30
NicoleMullins99 <175coco@gmail.com>
2024-11-20 13:03:12 +0000

    templets for ui

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 # Create a Flask application
 app = Flask(__name__)
@@ -6,7 +6,14 @@ app = Flask(__name__)
 # Define a route
 @app.route("/")
 def home():
-    return "Hello, Flask!"
+    return render_template("home.html")
+
+@app.route("/edit")
+def edit_page():
+    return render_template("edit.html")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
+
